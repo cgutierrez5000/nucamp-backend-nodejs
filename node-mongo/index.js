c13011694ee885bcebc04123c4f521b2c93d26a1
Carlos Gutierrez <c_e_gutierrez@yahoo.com>
2024-07-01 08:34:11 -0500

    Node Mongoose exercise

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,6 +1,5 @@
 const { MongoClient } = require('mongodb-legacy');
 const assert = require('assert');
-const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
 const dbName = 'nucampsite';
@@ -14,42 +13,24 @@ MongoClient.connect(url, {}, (err, client) => {
 
   db.dropCollection('campsites', (err, result) => {
     assert.strictEqual(err, undefined);
-    console.log('Dropped Collection:', result);
 
-    dboper.insertDocument(
-      db,
+    console.log('Dropped Collection', result);
+
+    const collection = db.collection('campsites');
+
+    collection.insertOne(
       { name: 'Breadcrumb Trail Campground', description: 'Test' },
-      'campsites',
-      (result) => {
+      (err, result) => {
+        assert.strictEqual(err, undefined);
+
         console.log('Insert Document:', result.ops);
 
-        dboper.findDocuments(db, 'campsites', (docs) => {
+        collection.find().toArray((err, docs) => {
+          assert.strictEqual(err, undefined);
+
           console.log('Found Documents:', docs);
 
-          dboper.updateDocument(
-            db,
-            { name: 'Breadcrumb Trail Campground' },
-            { description: 'Updated Test Description' },
-            'campsites',
-            (result) => {
-              console.log('Updated Document Count:', result.modifiedCount);
-
-              dboper.findDocuments(db, 'campsites', (docs) => {
-                console.log('Found Documents:', docs);
-
-                dboper.removeDocument(
-                  db,
-                  { name: 'Breadcrumb Trail Campground' },
-                  'campsites',
-                  (result) => {
-                    console.log('Deleted Document Count:', result.deletedCount);
-
-                    client.close();
-                  }
-                );
-              });
-            }
-          );
+          client.close();
         });
       }
     );
